cd3b2cf6517213dfc1c6cdb2aa29c73ebabfc55c
Corey Hawthorne <chawthorne02@gmail.com>
2022-09-30 21:34:25 -0400

    added nav bar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-
 import './App.css';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 
-function App() {
+
+function App() { 
   return (
-    <div className="App">
-    
-    </div>
+    <Navbar>
+    <Container fluid classname="navbar">
+    {/* <img src={pancakes} alt='logo' /> */}
+      <Navbar.Brand classname="title">The Brunch Spot</Navbar.Brand>
+        <Nav.Link href="#welcome">Welcome</Nav.Link>
+        <Nav.Link href="menu">Menu</Nav.Link>
+        <Nav.Link href="#">Order Now</Nav.Link>
+      
+    </Container>
+  </Navbar>
   );
 }
 
 export default App;
+ 
